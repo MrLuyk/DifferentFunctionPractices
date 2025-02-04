b719e9a37b4d3cafa8b66c16f7986d4cb78bfa7b
Mr. Luyk <133881094+MrLuyk@users.noreply.github.com>
2025-02-04 17:50:53 +0000

    Added a secret message txt file and created a function called FizzBuzz with a foreach in the main to make the printed line for Fizzbuzz look nice.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+public class Program
+{
+    static string sM;
+
+    public static void Main(string[] args)
+    {
+        sM = File.ReadAllText("/workspaces/DifferentFunctionPractices/secretMessage.txt");
+        int[] nums = sM.Split(" ").Select(n => Convert.ToInt32(n)).ToArray();
+
+        Console.Clear();
+        int count = 0;
+        foreach (string num in FizzBuzz(15))
+        {
+            if (count <= 13)
+            {
+                Console.Write($"{num}, ");
+            }
+            else
+            {
+                Console.Write($"{num}. ");
+            }
+            count++;
+        }
+        Console.WriteLine();
+    }
+
+    public static string[] FizzBuzz(int n)
+    {
+        string[] result = new string[n];
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+                result[i - 1] = "FizzBuzz";
+
+            else if (i % 3 == 0)
+                result[i - 1] = "Fizz";
+
+            else if (i % 5 == 0)
+                result[i - 1] = "Buzz";
+
+            else
+                result[i - 1] = "" + i;
+        }
+        return result;
+    }
+
+}
